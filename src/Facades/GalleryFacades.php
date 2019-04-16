190d0859f75ff8b43808ddcfbde5ff802e714b6d
Bojan Djurdjevic <47319947+BojanDjurdjevic995@users.noreply.github.com>
2019-04-16 13:07:15 +0200

    Update GalleryFacades.php

--- src/Facades/GalleryFacades.php
+++ src/Facades/GalleryFacades.php
@@ -1,9 +1,7 @@
 <?php
 namespace Baki\Gallery\Facades;
 
-use File;
 use Image;
-use App\Gallery;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Input; 
 use Illuminate\Support\Facades\Facade;
@@ -16,11 +14,11 @@ class GalleryFacades extends Facade
     {
         return 'StoreGallery';
     }
-    public static function store($picFromInput, $path, $idNews, $id_news='id_news', $width = false, $height = false)
+    public static function store($class, $input_name, $storage_disk, $id, $column, $edit = false)
     {
-        if (Input::has($picFromInput)) 
+        if (Input::has($input_name)) 
         {
-            $gallery = Input::file($picFromInput);
+            $gallery = Input::file($input_name);
             $insert = array();
             $allowedExtensions = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'];
             for ($i=0; $i < count($gallery); $i++) 
@@ -28,62 +26,45 @@ class GalleryFacades extends Facade
                 $image = $gallery[$i];
                 if ( in_array(File::extension($image->getClientOriginalName()), $allowedExtensions) ) 
                 {
-                    $file_name = time() . "-" . Str::random(5) . '-' . $image->getClientOriginalName();
-                    $location  = public_path($path . '' . $file_name);
-                    ($width && $height) ? (Image::make($image)->fit($width, $height)->save($location)) : (Image::make($image)->save($location));
-                    $insert[] = [$id_news => $idNews, 'image' => $path . '' . $file_name];
-                }
-            } 
-            Gallery::insert($insert);
-        }
-        return true;
-    }
-    public static function update($picFromInput, $path, $idNews, $id_news='id_news', $width = false, $height = false)
-    {
-        if (Input::has($picFromInput)) 
-        {
-            $gallery = Input::file($picFromInput);
-            $insert = array();
-            $allowedExtensions = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'];
-            for ($i=0; $i < count($gallery); $i++) 
-            { 
-                $image = $gallery[$i];
-                if ( in_array(File::extension($image->getClientOriginalName()), $allowedExtensions) ) 
-                {
-                    $file_name = time() . "-" . Str::random(5) . '-' . $image->getClientOriginalName();
-                    $location  = public_path($path . '' . $file_name);
-                    ($width && $height) ? (Image::make($image)->fit($width, $height)->save($location)) : (Image::make($image)->save($location));
-                    $insert[] = [$id_news => $idNews, 'image' => $path . '' . $file_name];
+                    $file_name = 'gallery-' . time() . "-" . strtolower(Str::random(5)) . '-' . $image->getClientOriginalName();
+                    $makeImage = Image::make($image);
+                    $path = Storage::disk($storage_disk)->path($file_name);
+                    Storage::disk($storage_disk)->put($file_name, $makeImage->save($path));
+                    $insert[] = [$column => $id, 'image' => $file_name];
                 }
             }
-            $oldGallery = Gallery::where($id_news, '=', $idNews)->get();
-            if (!empty($insert)) 
-            {   
-                $idPic=array();
-                foreach($oldGallery as $item)
-                {
-                    if(File::exists($item->image))
+            if($edit)
+            {
+                $galleryOld = $class::where($column, '=', $id)->get();
+                if (!empty($insert)) 
+                {   
+                    $idImage=array();
+                    foreach($galleryOld as $slika)
                     {
-                        $idPic[] = $item->id;
-                        File::delete($item->image);
+                        if(Storage::disk($storage_disk)->exists($slika->image))
+                        {
+                            $idImage[] = $slika->id;
+                            Storage::disk($storage_disk)->delete($slika->image);
+                        }
                     }
+                    $class::insert($insert);
+                    $class::destroy($idImage);
                 }
-                Gallery::insert($insert);
-                Gallery::destroy($idPic);
-            } 
+            }
+            else{
+                $class::insert($insert);
+            }   
         }
         return true;
     }
-    public static function delete($id, $columnName)
+    public static function delete($class, $id, $columnName, $storage_disk)
     {
-        $img = Gallery::where($columnName, '=', $id)->get();
-        $idPic=array();
-        foreach($img as $item)
+        $image = $class::where($columnName, '=', $id)->get();
+        foreach($image as $item)
         {
-            if(File::exists($item->image))
+            if(Storage::disk($storage_disk)->exists($item->image))
             {
-                File::delete($item->image);
-                $item->delete();    
+                Storage::disk($storage_disk)->delete($item->image);
             }
         } 
         return true;
